8bc9f43469a8dcbfa6e61d3d41ca23a0ebaab3f6
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2019-03-27 16:34:40 -0300

    add tests for `strLen`

--- src/main.c
+++ src/main.c
@@ -7,6 +7,16 @@
 
 #include "lib.h"
 
+void test_string(FILE *pfile){
+    fprintf(pfile, "test_string:\n");
+
+    // strLen:
+    fprintf(pfile, "strLen(\"gato\") = %d\n", strLen("gato"));
+    fprintf(pfile, "strLen(\"abc\") = %d\n", strLen("abc"));
+    fprintf(pfile, "strLen(\"\") = %d\n", strLen(""));
+
+}
+
 void test_n3tree(FILE *pfile){
     
 }
@@ -17,6 +27,7 @@ void test_nTable(FILE *pfile){
 
 int main (void){
     FILE *pfile = fopen("salida.caso.propios.txt","w");
+    test_string(pfile);
     test_n3tree(pfile);
     test_nTable(pfile);
     fclose(pfile);
