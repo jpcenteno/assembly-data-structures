86e322f7307085a82cfb51ed2d7604f83b16403d
Joaquín P. Centeno <jpcenteno@users.noreply.github.com>
2019-04-12 11:56:19 -0300

    Actualizo casos de prueba

--- src/tester.c
+++ src/tester.c
@@ -37,7 +37,7 @@ char* strings[10] = {"aa","bb","dd","ff","00","zz","cc","ee","gg","hh"};
 void test_strings(FILE *pfile) {
     char *a, *b, *c;
     // clone
-    fprintf(pfile,"==> Clone");
+    fprintf(pfile,"==> Clone\n");
     a = strClone("casa");
     b = strClone("");
     strPrint(a,pfile);
@@ -47,16 +47,25 @@ void test_strings(FILE *pfile) {
     strDelete(a);
     strDelete(b);
     // concat
-    fprintf(pfile,"==> Concat");
+    fprintf(pfile,"==> Concat\n");
     a = strClone("perro_");
     b = strClone("loco");
     fprintf(pfile,"%i\n",strLen(a));
     fprintf(pfile,"%i\n",strLen(b));
     c = strConcat(a,b);
+    strPrint(c,pfile);
+    fprintf(pfile,"\n");
     c = strConcat(c,strClone(""));
+    strPrint(c,pfile);
+    fprintf(pfile,"\n");
     c = strConcat(strClone(""),c);
+    strPrint(c,pfile);
+    fprintf(pfile,"\n");
+    c = strConcat(c,c);
+    strPrint(c,pfile);
+    fprintf(pfile,"\n");
     // range
-    fprintf(pfile,"==> Range");
+    fprintf(pfile,"==> Range\n");
     fprintf(pfile,"%i\n",strLen(c));
     int h = strLen(c);
     for(int i=0; i<h+1; i++) {
@@ -71,7 +80,7 @@ void test_strings(FILE *pfile) {
     }
     strDelete(c);
     // cmp
-    fprintf(pfile,"==> Cmp");
+    fprintf(pfile,"==> Cmp\n");
     char* texts[5] = {"sar","23","taaa","tbb","tix"};
     for(int i=0; i<5; i++) {
         for(int j=0; j<5; j++) {
@@ -85,35 +94,49 @@ void test_list(FILE *pfile) {
     char *a, *b, *c;
     list_t* l1;
     // listAddFirst
-    fprintf(pfile,"==> listAddFirst");
-    l1 = listNew();
+    fprintf(pfile,"==> listAddFirst\n");
+    l1 = listNew();    
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listAddFirst(l1,strClone("PRIMERO"));
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     l1 = listNew();
     listAddFirst(l1,strClone("PRIMERO"));
     listAddFirst(l1,strClone("PRIMERO"));
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     l1 = listNew();
     listAddFirst(l1,strClone("PRIMERO"));
     listAddFirst(l1,strClone("PRIMERO"));
     listAddFirst(l1,strClone("PRIMERO"));
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     // listAddLast
-    fprintf(pfile,"==> listAddLast");
+    fprintf(pfile,"==> listAddLast\n");
     l1 = listNew();
     listAddLast(l1,strClone("ULTIMO"));
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     l1 = listNew();
     listAddLast(l1,strClone("ULTIMO"));
     listAddLast(l1,strClone("ULTIMO"));
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     l1 = listNew();
     listAddLast(l1,strClone("ULTIMO"));
     listAddLast(l1,strClone("ULTIMO"));
     listAddLast(l1,strClone("ULTIMO"));
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     // listAdd
-    fprintf(pfile,"==> listAdd");
+    fprintf(pfile,"==> listAdd\n");
     l1 = listNew();
     for(int i=0; i<5;i++)
         listAdd(l1,strClone(strings[i]),(funcCmp_t*)&strCmp);
@@ -131,7 +154,7 @@ void test_list(FILE *pfile) {
     listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     // listRemove
-    fprintf(pfile,"==> listRemove");
+    fprintf(pfile,"==> listRemove\n");
     l1 = listNew();
     listRemove(l1, strings[0], (funcCmp_t*)&strCmp, (funcDelete_t*)&strDelete);
     for(int i=0; i<5;i++) {
@@ -148,7 +171,7 @@ void test_list(FILE *pfile) {
     listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     // listRemoveFirst
-    fprintf(pfile,"==> listRemove");
+    fprintf(pfile,"==> listRemoveFirst\n");
     l1 = listNew();
     listRemoveFirst(l1, (funcDelete_t*)&strDelete);
     for(int i=0; i<5;i++)
@@ -163,7 +186,7 @@ void test_list(FILE *pfile) {
     listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
     // listRemoveLast
-    fprintf(pfile,"==> listRemove");
+    fprintf(pfile,"==> listRemoveLast\n");
     l1 = listNew();
     listRemoveLast(l1, (funcDelete_t*)&strDelete);
     for(int i=0; i<5;i++)
@@ -177,6 +200,49 @@ void test_list(FILE *pfile) {
     listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
     listDelete(l1,(funcDelete_t*)&strDelete);
+    // listRemove listRemoveFirst listRemoveLast
+    fprintf(pfile,"==> listRemove listRemoveFirst listRemoveLast\n");
+    l1 = listNew();
+    listRemove(l1, strings[2], (funcCmp_t*)&strCmp, 0);
+    listRemoveFirst(l1, 0);
+    listRemoveLast(l1, 0);
+    char* stringsLocal[10];
+    for(int i=0; i<10;i++)
+        stringsLocal[i] = strClone(strings[i]);
+    for(int i=0; i<10;i++)
+        listAdd(l1,stringsLocal[i],(funcCmp_t*)&strCmp);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemove(l1, strings[2], (funcCmp_t*)&strCmp, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemoveLast(l1, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemoveFirst(l1, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemoveLast(l1, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemove(l1, strings[2], (funcCmp_t*)&strCmp, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemoveFirst(l1, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemoveLast(l1, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemoveFirst(l1, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listRemove(l1, strings[2], (funcCmp_t*)&strCmp, 0);
+    listPrint(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listPrintReverse(l1,pfile,(funcPrint_t*)&strPrint); fprintf(pfile,"\n");
+    listDelete(l1,0);
+    for(int i=0; i<10;i++)
+        free(stringsLocal[i]);
 }
 
 /** n3Tree **/
@@ -184,7 +250,7 @@ void test_n3Tree(FILE *pfile) {
     char *a, *b, *c;
     n3tree_t *t;
     // n3treeAdd
-    fprintf(pfile,"==> n3treeAdd");
+    fprintf(pfile,"==> n3treeAdd\n");
     t = n3treeNew();
     n3treeAdd(t, strClone("zar"),(funcCmp_t*)&strCmp);
     n3treeAdd(t, strClone("zar"),(funcCmp_t*)&strCmp);
@@ -195,7 +261,7 @@ void test_n3Tree(FILE *pfile) {
     n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
     fprintf(pfile,"\n");
     // n3treeRemoveEq
-    fprintf(pfile,"==> n3treeRemoveEq");
+    fprintf(pfile,"==> n3treeRemoveEq\n");
     n3treeRemoveEq(t,(funcDelete_t*)&strDelete);
     n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
     fprintf(pfile,"\n");
@@ -213,11 +279,30 @@ void test_n3Tree(FILE *pfile) {
     n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
     fprintf(pfile,"\n");
     // n3treeRemoveEq
-    fprintf(pfile,"==> n3treeRemoveEq");
+    fprintf(pfile,"==> n3treeRemoveEq\n");
     n3treeRemoveEq(t,(funcDelete_t*)&strDelete);
     n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
     fprintf(pfile,"\n");
     n3treeDelete(t,(funcDelete_t*)&strDelete);
+    fprintf(pfile,"==> n3treeDelete n3treeRemoveEq\n");
+    char* stringsLocal[20];
+    t = n3treeNew();
+    for(int i=0; i<10;i++) {
+        stringsLocal[i] = strClone(strings[i]);
+        stringsLocal[i+10] = strClone(strings[i]);
+    }
+    n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
+    for(int i=0; i<10;i++)
+        n3treeAdd(t, stringsLocal[i],(funcCmp_t*)&strCmp);
+    n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
+    for(int i=10; i<20;i++)
+        n3treeAdd(t, stringsLocal[i],(funcCmp_t*)&strCmp);
+    n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
+    n3treeRemoveEq(t,0);
+    n3treePrint(t,pfile,(funcPrint_t*)&strPrint);
+    n3treeDelete(t,0);
+    for(int i=0; i<20;i++)
+        free(stringsLocal[i]);
 }
 
 /** nTable **/
@@ -225,25 +310,25 @@ void test_nTable(FILE *pfile) {
     char *a, *b, *c;
     nTable_t *n;
     // nTableAdd
-    fprintf(pfile,"==> nTableAdd");
-    n = nTableNew(64);
+    fprintf(pfile,"==> nTableAdd\n");
+    n = nTableNew(32);
     for(int s=0;s<64;s++) {
         for(int i=0;i<10;i++) {
             nTableAdd(n, s, strClone(strings[i]), (funcCmp_t*)&strCmp);}}
     // nTableRemoveSlot
-    fprintf(pfile,"==> nTableRemoveSlot");
+    fprintf(pfile,"==> nTableRemoveSlot\n");
     for(int i=0;i<10;i++) {
         a = strClone(strings[i]);
         nTableRemoveSlot(n,i,a,(funcCmp_t*)&strCmp,(funcDelete_t*)&strDelete);
         strDelete(a);}
     // nTableRemoveAll
-    fprintf(pfile,"==> nTableRemoveAll");
+    fprintf(pfile,"==> nTableRemoveAll\n");
     for(int i=5;i<10;i++) {
         a = strClone(strings[i]);
         nTableRemoveAll(n,a,(funcCmp_t*)&strCmp,(funcDelete_t*)&strDelete);
         strDelete(a);}
     // nTableDeleteSlot
-    fprintf(pfile,"==> nTableDeleteSlot");
+    fprintf(pfile,"==> nTableDeleteSlot\n");
     for(int i=50;i<56;i++) {
         nTableDeleteSlot(n,i,(funcDelete_t*)&strDelete);}
     nTablePrint(n,pfile,(funcPrint_t*)&strPrint);
@@ -268,7 +353,7 @@ void test_2(char* filename){
     RUN(filename, listPrint(l, pfile, (funcPrint_t*)&strPrint);)NL(filename)
     RUN(filename, n3treePrint(t, pfile, (funcPrint_t*)&strPrint);)NL(filename)
     RUN(filename, nTablePrint(n, pfile, (funcPrint_t*)&strPrint);)NL(filename)
-    for(int s=0;s<32;s++) {
+    for(int s=0;s<40;s++) {
         for(int i=0;i<20;i++) {
             for(int j=0;j<20;j++) {
                 listAdd(l, strConcat(strClone(text[i]), strClone(text[j])), (funcCmp_t*)&strCmp);
@@ -311,7 +396,7 @@ void test_3(char* filename){
     RUN(filename, listPrint(l, pfile, (funcPrint_t*)&strPrint);)NL(filename)
     RUN(filename, n3treePrint(t, pfile, (funcPrint_t*)&strPrint);)NL(filename)
     RUN(filename, nTablePrint(n, pfile, (funcPrint_t*)&strPrint);)NL(filename)
-    for(int s=0;s<32;s++) {
+    for(int s=0;s<40;s++) {
         for(int i=0;i<1000;i++) {
                 listAdd(l, randomString(rand()%10+1), (funcCmp_t*)&strCmp);
                 n3treeAdd(t, randomString(rand()%10+1), (funcCmp_t*)&strCmp);
